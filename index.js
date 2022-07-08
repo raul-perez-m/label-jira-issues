3715969752d9f18e565f4d1f63646b22b9e35c5d
Raul Perez <raul272@gmail.com>
2022-07-08 16:06:12 +0200

    Version 1.0.0 Label jira issues.

--- index.js
+++ index.js
@@ -1,21 +1,29 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
 const {LabelJiraIssues} = require("./label-jira-issues");
 
 const sRepo = github.context.repo.repo;
 const sOwner = github.context.repo.owner
 const sVersion = core.getInput("version")
-const sAuthToken = core.getInput("github-access-token");
+const sAuthToken = core.getInput("github-access-token");  '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'
 const environment = core.getInput("environment");
+const jiraConfig ={
+    jiraUser: core.getInput("jira-user"),
+    jiraPassword: core.getInput("jira-password"),
+    jiraUrl: core.getInput("jira-url")
+}; 
 
 console.log(`version: ${sVersion}`);
 if (!sRepo) { core.error("no repository specified, aborting"); }
 if (!sOwner) { core.error("no owner specified, aborting"); }
 if (!sVersion) { core.error("no version specified, aborting"); }
 if (!sAuthToken) { core.error("no GitHub access token specified, aborting"); }
+if (!environment) { core.error("no environment specified, aborting"); }
+if (!jiraConfig.jiraUser) { core.error("no jira user specified, aborting"); }
+if (!jiraConfig.jiraPassword) { core.error("no jira password specified, aborting"); }
+if (!jiraConfig.jiraUrl) { core.error("no jira url specified, aborting"); }
 
 const run = async function () {
-    new LabelJiraIssues(sAuthToken, sOwner, sRepo, sVersion, environment).labelJiraIssues();
+    new LabelJiraIssues(sAuthToken, sOwner, sRepo, sVersion, environment, jiraConfig).labelJiraIssues();
 }
 
 try {
--- label-jira-issues.js
+++ label-jira-issues.js
@@ -1,31 +1,56 @@
 const { Octokit } = require("@octokit/rest");
+const core = require('@actions/core');
+const axios = require("axios");
 
 module.exports.LabelJiraIssues = class LabelJiraIssues {
 
-    constructor(sAuthToken, sOwner, sRepo, sVersion) {
+    constructor(sAuthToken, sOwner, sRepo, sVersion, environment, jiraConfig) {
         this.owner = sOwner;
         this.repo = sRepo;
-        this.version = sVersion;
-
+        const version = sVersion.match(/v\d*/gi);
+        this.version = version[0];
+        this.jiraConfig = jiraConfig;
         this.octokit = new Octokit({
             auth: sAuthToken,
         });
+        this.environment = environment;
     }
 
     async labelJiraIssues() {
         const release = await this.getReleaseInfos(this.owner, this.repo);
+
         await this.updateIssues(release);
     }
 
-    async getReleaseInfos (sOwner, sRepo) {
+    async getReleaseInfos(sOwner, sRepo) {
         const { data } = await this.octokit.request(`/repos/${sOwner}/${sRepo}/releases`);
         const oCurrentRelease = data.find(oRelease => oRelease.tag_name.includes(this.version));
         return oCurrentRelease;
     }
-    async updateIssues (release) {
+    async updateIssues(release) {
         const description = release.body;
         const issues = description.match(/FCA[-|\s]\d*/gi);
         console.log(`Found ${issues.length} issues`);
+
+        for (const issue of issues) {
+            const issueNumber = issue.replace(/([ ])/g, '-');
+            const request = {
+                method: 'PUT',
+                url: `${this.jiraConfig.jiraUrl}/rest/api/latest/issue/${issueNumber}`,
+                auth: {
+                    username: this.jiraConfig.jiraUser,
+                    password: this.jiraConfig.jiraPassword,
+                },
+                data: { fields: { labels: [this.environment] } }
+            };
+            try {
+                await axios(request);
+                console.log(`${issueNumber} updated with TAG: ${this.environment}`);
+            } catch (error) {
+                console.log(error);
+                core.error(`${issueNumber} not updated with TAG: ${this.environment}`);
+            }
+        }
     }
 
 }
